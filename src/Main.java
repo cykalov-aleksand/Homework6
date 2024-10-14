e5ecbb3d40bbde99f94de06b11b5ce065bfe450b
1в <cykalov.aleksand@mail.ru>
2024-10-14 20:19:58 +0300

    устранил ошибки переменным дал имена, которые отражают их суть

--- src/Main.java
+++ src/Main.java
@@ -2,48 +2,48 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("------------------------Задание №1---------------------------");
         System.out.println("Вывести целые числа от 1 до 10");
-        for (byte i = 1; i <= 10; i++) {
-            System.out.println("Целое число: " + i);
+        for (byte number = 1; number <= 10; number++) {
+            System.out.println("Целое число: " + number);
         }
         System.out.println("\n------------------------Задание №2---------------------------");
         System.out.println("Вывести целые числа от 10 до 1");
-        for (byte i = 10; i >= 1; i--) {
-            System.out.println("Целое число: " + i);
+        for (byte number = 10; number >= 1; number--) {
+            System.out.println("Целое число: " + number);
         }
         System.out.println("\n------------------------Задание №3---------------------------");
         System.out.println("Вывести в консоль все четные числа от 0 до 17");
-        for (byte i = 0; i <= 17; i =(byte) (i + 2)) {
-            System.out.println("Чётное число: " + i);
+        for (byte number = 0; number <= 17; number =(byte) (number + 2)) {
+            System.out.println("Чётное число: " + number);
         }
         System.out.println("\n------------------------Задание №4---------------------------");
         System.out.println("Вывести в консоль все числа от 10 до -10");
-        for (byte i = 10; i >= -10; i--) {
-            System.out.println("Число: " + i);
+        for (byte number = 10; number >= -10; number--) {
+            System.out.println("Число: " + number);
         }
         System.out.println("\n------------------------Задание №5---------------------------");
         System.out.println("Вывести в консоль все високосные года от 1904 до 2096");
-        for (short i=1904; i <= 2096; i = (short) (i + 4)) {
-         System.out.println(i + " год является високосным");
+        for (short year=1904; year <= 2096; year = (short) (year + 4)) {
+         System.out.println(year + " год является високосным");
         }
         System.out.println("\n------------------------Задание №6---------------------------");
         System.out.println("Вывести в консоль последовательность чисел от 7 до 98 с шагом 7");
         System.out.print("последовательность чисел: ");
-        for (byte i = 7; i <= 98; i =(byte) (i + 7)) {
-            System.out.print(" " + i);
+        for (byte number = 7; number <= 98; number =(byte) (number + 7)) {
+            System.out.print(" " + number);
         }
         System.out.println("\n\n------------------------Задание №7---------------------------");
         System.out.println("Вывести в консоль последовательность чисел от 2 до 512 с шагом умножение на 2");
         System.out.print("последовательность чисел: ");
-        for (short i = 2; i <= 512; i =(short) (i * 2)) {
-            System.out.print(" " + i);
+        for (short number = 2; number <= 512; number =(short) (number * 2)) {
+            System.out.print(" " + number);
         }
         System.out.println("\n\n------------------------Задание №8---------------------------");
         float total = 0f;
         float salary = 29000f;
         System.out.println("Вывести в консоль ежемесячное накопление в течении года, если каждый месяц откладывать по " + salary + " руб.");
-        for (byte i = 1; i <= 12; i++) {
+        for (byte month = 1; month <= 12; month++) {
             total = total + salary;
-            System.out.println("Месяц " + i + ", сумма накоплений равна " + total+" руб.");
+            System.out.println("Месяц " + month + ", сумма накоплений равна " + total+" руб.");
         }
         System.out.println("\n------------------------Задание №9---------------------------");
         total = 0f;
@@ -51,9 +51,9 @@ public class Main {
         System.out.println("Вывести в консоль ежемесячное накопление в течении года, если каждый месяц откладывать по " + salary +
                 " руб. и ежегодный процент накоплений составляет " + percentageDeposit + " %");
         byte numberMonths=12;
-        for (byte i = 1; i <= 12; i++) {
+        for (byte month = 1; month <= 12; month++) {
             total = total + (salary + (salary * percentageDeposit / (numberMonths * 100f)));
-            System.out.println("Месяц " + i + ", сумма накоплений равна " + total+" руб.");
+            System.out.println("Месяц " + month + ", сумма накоплений равна " + total+" руб.");
         }
         System.out.println("\n------------------------Задание №10---------------------------");
         System.out.println("Таблица умножения на 2");
